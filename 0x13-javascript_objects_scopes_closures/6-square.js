e3067d1f23e32e4fa4cb823abb14e44fd8dce76d
MichaelOppong731 <87698777+MichaelOppong731@users.noreply.github.com>
2022-09-06 20:53:08 +0000

    update

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
-
 const Rectangle = require('./4-rectangle');
 
 class Square extends Rectangle {
@@ -8,10 +7,13 @@ class Square extends Rectangle {
   }
 
   charPrint (c) {
-    if (c === undefined) c = 'X';
-
-    for (let i = 0; i < this.height; i++) {
-      console.log(c.repeat(this.height));
+    if (!c) {
+      c = 'X';
+    }
+    let counter = 0;
+    while (counter < this.height) {
+      console.log(c.repeat(this.width));
+      counter++;
     }
   }
 }
