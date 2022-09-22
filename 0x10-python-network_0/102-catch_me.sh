3f6e12e16bcb7d2c66246476285b21d690a287a1
MichaelOppong731 <87698777+MichaelOppong731@users.noreply.github.com>
2022-09-22 23:24:41 +0000

    Networking Basics

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Script that causes the server to respond with a message containing.
-curl -sL -X PUT 0.0.0.0:5000/catch_me -d "user_id=98" -H "You got me!"
+# Makes a request to 0.0.0.0:5000/catch_me that gets the message "You got me!".
+curl -sL -X PUT -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
